af41737ee6fe6ec9fa6df6ddf980c300cde3c862
Lister Miselo <talislister1@gmail.com>
2022-11-30 05:01:40 -0800

    Fixed

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -10,33 +10,22 @@
 int check_cycle(listint_t *list)
 
 {
-
 	listint_t *slow, *fast;
 
 	if (!list)
-
 	{
 		return (0);
 	}
-
 	slow = list;
-
 	fast = list->next;
-
 	while (fast && slow && fast->next)
-
 	{
 		if (slow == fast)
-
 		{
 			return (1);
-
 		}
-
 		slow = slow->next;
-
 		fast = fast->next->next;
 	}
-
 	return (0);
 }
