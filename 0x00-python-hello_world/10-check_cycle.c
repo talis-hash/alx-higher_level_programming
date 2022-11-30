d56e3a38868331632b6a91ff1a1146f601476e0b
Lister Miselo <talislister1@gmail.com>
2022-11-30 04:41:55 -0800

    Final fix

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,17 +1,11 @@
 #include "lists.h"
 
 /**
-
  * check_cycle - check for loop in LL
-
  * @list: head of linked list
-
  * Description - check for loops in LL
-
  * Return: 1 if cycled, 0 if not
-
  */
-
 
 int check_cycle(listint_t *list)
 
