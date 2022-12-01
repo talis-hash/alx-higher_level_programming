e0b510112890ad872b4bc71f90e1a68e71650c29
Lister Miselo <talislister1@gmail.com>
2022-12-01 12:53:10 -0800

    fixed

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,10 +3,10 @@ import hidden_4
 
 def discovr():
     name = dir(hidden_4)
-        for i in name:
-            if i[:2] != '__':
-                print("{:s}".format(i))
+    for i in name:
+        if i[:2] != '__':
+            print("{:s}".format(i)
 
 
-    if __name__ == "__main__":
-        discovr()
+if __name__ == "__main__":
+    discovr()
